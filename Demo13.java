476a760513a9b7c7af7b18e1bde013de0293b4c7
Kavindu95 <kavindueng95@gmail.com>
2023-08-08 21:40:35 +0530

    demo13

--- Demo13.java
+++ Demo13.java
@@ -1,46 +1,58 @@
 public class Demo13 {
     public static void main(String[] args) {
-        
-        /*Loops */
+        /* Loops */
 
-        /*While loops */
+        // System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+        // System.out.println("Hello World!");
 
-        // System.out.println("Hello world");
-        // System.out.println("Hello world");
-        // System.out.println("Hello world");
-        // System.out.println("Hello world");
-        // System.out.println("Hello world");
-        // System.out.println("Hello world");
-        /*while (boolean expression) statement;
-         * 
-         * while (boolean expression){
-         * statement(s)..
-         * }
+        /* 
+            while (boolean expression) statement;
+
+            while (boolean expression) {
+                statement(s)...
+            }
          */
-        //while (true) System.out.println("Hello world");
-        int x=0;
-        while(x++<5){
-            System.out.println("Hello world");
+
+         int x = 0;
+         boolean flag = true;
+         while (flag) {
+            System.out.println("Hello World");
+            x++;
+            if (x == 5) flag = false;
+         }
+         System.out.println();
+
+         int y = 0;
+         while (y++ < 5){
+            System.out.println("Hello World");
+         }
+         System.out.println();
+
+         int z = 0;
+         while (z < 5) {
+            System.out.println("Hello World!");
+            z++;
+         }
+         System.out.println();
+
+         int r = 0;
+         while (true){
+            System.out.println("Hello World");
+            if (++r == 5)  break;
+         }
+
+        // System.out.println("Loops-1");
+        // System.out.println("Loops-2");
+        // System.out.println("Loops-3");
+        // System.out.println("Loops-4");
+        // System.out.println("Loops-5");
+
+        int a = 1;
+        while (a <= 5){
+            System.out.println("Loops-" + a++);
         }
-        int y=0;
-        while(y<5){
-            System.out.println("Hi DEP-11");
-            y++;
-        }
-
-
-
-        // System.out.println("LOOPS-1");
-        // System.out.println("LOOPS-2");
-        // System.out.println("LOOPS-3");
-        // System.out.println("LOOPS-4");
-        // System.out.println("LOOPS-5");
-        // System.out.println("LOOPS-6");
-
-        int z=1;
-        while(z<=6){
-            System.out.println("Loops"+z++);
-        }
-        
     }
 }
