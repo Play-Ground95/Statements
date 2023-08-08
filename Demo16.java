8b27a3f0e6ba2327f659884ab15355b3af9d1c3c
Kavindu95 <kavindueng95@gmail.com>
2023-08-08 21:45:31 +0530

    demo16

---
+++ Demo16.java
@@ -0,0 +1,80 @@
+public class Demo16 {
+    public static void main(String[] args) {
+        
+        /* 
+            Single Loop, Nested Loops
+            Via +
+
+            + + + + +
+            + + + +
+            + + +
+            + +
+            +                           
+            + +
+            + + +
+            + + + +
+            + + + + +
+         */
+
+         /*
+            Single Loop, Nested Loops
+            Via +
+
+            + + + + + + +
+            + + +   + + +
+            + +       + +
+            +           +
+
+          */
+
+         /*
+            Nested Loops
+            Via + (Space)
+
+
+            + + + + + + +
+            + + +   + + +
+            + +       + +
+            +           +
+            + +       + +
+            + + +   + + +
+            + + + + + + +
+          */          
+
+         /* 
+            Nested Loops
+            Via + / \
+
+
+            + + + + + +
+            + + / \ + +
+            + / / \ \ +
+            + \ \ / / +
+            + + \ / + +
+            + + + + + +
+          */
+
+          /*
+            Nested Loops
+            Via + / \
+
+            + + + + + 
+            + / + \ +
+            + + + + +
+            + \ + / +
+            + + + + +
+
+           */
+
+           /*
+            Nested Loops
+            Via + Space
+
+                + 
+              + + +
+            + + + + + 
+              + + + 
+                + 
+            */
+    }
+}
