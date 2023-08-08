6fec52e1636e539b44d33a8b2639ebf76f770f69
Kavindu95 <kavindueng95@gmail.com>
2023-08-08 21:30:37 +0530

    demo7

--- Demo7.java
+++ Demo7.java
@@ -5,17 +5,22 @@ public class Demo7 {
     private static final Scanner scanner =new Scanner(System.in);
     public static void main(String[] args) {
 
-        
-        //
-        //
-        //
-    System.out.print("Enter your name");
-    String name=scanner.nextLine();
-    System.out.printf("Hi! Welcome %s \n",name);
+        // Standard Output  -> Screen       -> System.out
+        // Standard Error   -> Screen       -> System.err
+        // Standard Input   -> Keyboard     -> System.in
 
-    System.out.print("Enter your age");
-    String age =scanner.nextLine();
-    System.out.printf("Your age is %s \n",age);
+        // System.out.println("Hello");
+        // System.err.println("IJSE");
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.printf("Hi! Welcome %s \n", name);
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.printf("Your age is: %s \n", age);
+
+        // scanner.nextDouble();
     
     }
 }
