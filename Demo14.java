57fe181567f839c4b551f6513891d1fb92efb775
Kavindu95 <kavindueng95@gmail.com>
2023-08-08 21:42:53 +0530

    demo14

--- Demo14.java
+++ Demo14.java
@@ -1,25 +1,31 @@
 public class Demo14 {
     public static void main(String[] args) {
-        /*
-         * + + + + +
-         * + + + + +
-         * + + + + +
-         * + + + + +
-         * + + + + +
+        
+        /* 
+            + + + + +
+            + + + + + 
+            + + + + +
+            + + + + +
+            + + + + +
          */
 
-         int x=0;
-         int y=0;
-         
-         while(y++<5){
-                while(x++<5){
-                System.out.print("+ ");  
+         int y = 0;
+         while (y++ < 5) {
+
+            int x = 0;
+            while (x++ < 5){
+                System.out.print("+ "); //  + + + + +
             }
             System.out.println();
-            
-        }
-        /////////////////////////////////////
 
-        
+        }
+
+        System.out.println();
+
+        int k = 0;
+        while (k++ < 25){
+            System.out.print("+ ");
+            if (k % 5 == 0) System.out.println();
+        }
     }
 }
