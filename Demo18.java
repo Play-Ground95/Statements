fc550036b3fc6d4bbe121be3136cc4fc747bac77
Kavindu95 <kavindueng95@gmail.com>
2023-08-09 22:23:25 +0530

    Demo19

--- Demo18.java
+++ Demo18.java
@@ -1,5 +1,46 @@
 public class Demo18 {
     public static void main(String[] args) {
+          /* 
+            Nested Loops
+            Via + / \
+
+
+            + + + + + +
+            + + / \ + +
+            + / / \ \ +
+            + \ \ / / +
+            + + \ / + +
+            + + + + + +
+          */
         
+          int width = 10;
+          int y = 0;
+          int k = 0;
+          while (y++ < width){
+  
+              int x = 0;
+              while (x++ < width){
+                  if (x > ((width/2)-k) && x <= ((width/2)+k)){
+                      
+                      if (x > ((width/2)-k) && x <= (width/2) && y <= (width/2) || 
+                          x <= ((width/2)+k) && x > (width/2) && y >= ((width/2) + 1)){
+                          System.out.print("/ ");
+                      }else{
+                          System.out.print("\\ ");
+                      }
+  
+                  }else{
+                      System.out.print("+ ");
+                  }
+              }
+  
+              if (y < (width/2)){
+                  k++;
+              }else if(y >= (width/2 + 1)){
+                  k--; 
+              }
+              
+              System.out.println();
     }
+}
 }
--- Demo19.java
+++ Demo19.java
@@ -1,70 +1,86 @@
 public class Demo19 {
     public static void main(String[] args) {
-        /*
-         do{
-            statement
-            statement
-            statement
-         }while(boolean expression)
-         */
-        // int x=0;
-        //  do System.out.println("hello"); while(x++ <5);
-
-         /*
-          + + + + +
-          + + + +
-          + + +
-          + +
-          +
-          */
-
-        //   int y=0;
-        //   do{
-         
-        //   int x=0;
-        //   do {
-        //     System.out.print(x);
-       
-        //     }while(x++ <4-y);
-        //      System.out.println();
-          
-        // }while(y++ <4);
-
-        ////////////////////////////////////////////////////
-
-            // int x=0;
-            // int y=0;
-            // do{
-            //     System.out.print(x);
-            //     x=4-y;
-            //     y++;
-
-            // }while(x++ <4); //incorrect
-            ///////////////////////////////////////////////
-           
-            /*
-            + + + + +
-            + + + +
-            + + +
-            + +
-            +                           
-            + +
-            + + +
-            + + + +
-            + + + + +
-             */
-
-            int x=1;
-            int y=0;
+      /* 
+            do statement while(boolean expression);
 
             do{
+                statement
+                statement
+                statement
+            } while(boolean expression);
+         */
+
+         int k = 0;
+         do System.out.println("Hello"); while(k++ < 5);
+
+         /* 
+            + + + + +
+            + + + +
+            + + +
+            + +
+            +
+          */
+        
+        int y = 0;
+
+        do{
+
+            k = 0;
+            do System.out.print("+ "); while (k++ < (4-y));
+
+            System.out.println();
+
+        }while(y++ < 4);
+
+        System.out.println("\n\n");
+
+        k = 0;
+        int x = 0;
+        y = 0;
+        do {
+
+            x = k - y * 5; 
+            if (x < 5-y) System.out.print("+ ");
+
+            if ((k+1) % 5 == 0) {
+                System.out.println();
+                y++;
+            }
+        }while (k++ < 24);
+
+        /*
+            + + + + +
+            + + + +
+            + + +
+            + +
+            +
+            + +
+            + + +
+            + + + +
+            + + + + +
+
+         */
+
+         System.out.println();
+         k = 0;
+         x = 0;
+         y = 0;
+         int j = -1;
+         int m = 5;
+
+         do {
+            x = k - y * 5;
+            if (x < m){
                 System.out.print("+ ");
-                if (x%5==0) {
-                    System.out.println();
-                }
+            }
 
-            }while (x++ <45);
-
+            if ((k+1) % 5 == 0){
+                System.out.println();
+                y++;
+                m += j;
+                if (m == 1) j = 1;
+            } 
+         } while (k++ < 44);
 
     }
 }
